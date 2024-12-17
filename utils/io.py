1cf5c0a7422276b5963450f9985e2a33d5489667
Scott Huberty <seh33@uw.edu>
2024-12-17 10:42:02 -0800

    DOC: add docstring

--- utils/io.py
+++ utils/io.py
@@ -66,3 +66,77 @@ def get_brainvol_df(fname, zscore=False):
         var_name="region",
     )
     return df_long.set_index("study_id")
+
+
+def get_aparc_long_format(fname):
+    """Read either the lh.aparc.stats or rh.aparc.stats file into a long format DataFrame.
+
+    Parameters
+    ----------
+    fname : str | pathlib.Path
+        The path to the lh.aparc.stats or rh.aparc.stats file.
+        For example ``aparc_newborn_lh.csv`` or ``aparc_newborn_rh.csv``.
+
+    Returns
+    -------
+    pd.DataFrame
+        A long format DataFrame containing the data from either hemisphere.
+
+    Notes
+    -----
+    This function is designed to work with the
+    ``aparc_{newborn, sixmonth}_{lh, rh}.csv`` files that typically reside in the
+    ``csv`` directory of this project. If you don't have these files, you can generate
+    them by running the scripts/get_freesurfer_stats.py script, but note that you need
+    to be run this script on a computer that is mounted to the Lab server.
+
+    Examples
+    --------
+    >>> fname = "aparc_newborn_lh.csv"
+    >>> df = get_aparc_long_format(fname)
+    """
+
+    df = pd.read_csv(fname)
+    return df.melt(
+        id_vars=["study_id", "StructName"],
+        var_name="metric",
+    )
+
+
+def get_aparc_all_hemisphere(fname_lh, fname_rh):
+    """Read the csv file containing each subjects Freesurfer {lh,rh}.aparc.stats data.
+
+    Parameters
+    ----------
+    fname_lh : str | pathlib.Path
+        The path to the lh.aparc.stats file. For example ``aparc_newborn_lh.csv``.
+    fname_rh : str | pathlib.Path
+        The path to the rh.aparc.stats file. For example ``aparc_newborn_rh.csv``.
+
+    Returns
+    -------
+    pd.DataFrame
+        A long format DataFrame containing the data from both hemispheres.
+
+    Notes
+    -----
+    This function is designed to work with the
+    ``aparc_{newborn, sixmonth}_{lh, rh}.csv`` files that typically reside in the
+    ``csv`` directory of this project. If you don't have these files, you can generate
+    them by running the scripts/get_freesurfer_stats.py script, but note that you need
+    to be run this script on a computer that is mounted to the Lab server.
+
+    Examples
+    --------
+    >>> fname_lh = "aparc_newborn_lh.csv"
+    >>> fname_rh = "aparc_newborn_rh.csv"
+    >>> df_hemis = get_aparc_all_hemisphere(fname_lh, fname_rh)
+    """
+    df_lh = get_aparc_long_format(fname_lh)
+    df_lh["hemisphere"] = "lh"
+    df_lh["StructName"] = df_lh["StructName"].str.replace("lh-", "")
+    df_rh = get_aparc_long_format(fname_rh)
+    df_rh["hemisphere"] = "rh"
+    df_rh["StructName"] = df_rh["StructName"].str.replace("rh-", "")
+    df_hemis = pd.concat([df_lh, df_rh])
+    return df_hemis
